bdc282453df892be72340d80a0bc10063aec52b6
vaishkiyarminal001 <vaishkiyarminal001@gmail.com>
2022-12-09 22:38:50 +0530

    check the code

---
+++ prime.js
@@ -0,0 +1,12 @@
+let prime=0;
+let num=99;
+for(let i=1; i>==num; i++){
+    if(num%i==0){
+        prime++;
+    }
+}
+if(prime==2){
+    console.log(num,"It is a prime")
+}else{
+    console.log(num,"It is Not a prime");
+}
